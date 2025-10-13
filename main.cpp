fc3c86f586d1c9b4805babc79abf6db3cac0f0a7
Mortalzs <kleyzormax7@gmail.com>
2025-10-13 18:21:02 +0000

    task 1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
 void sumEvenNumbers() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    if (a > b) swap(a, b);
+
     int sum = 0;
-    for (int i = 2; i <= 10; i += 2)
-        sum += i;
-    cout << "Sum of even numbers = " << sum << endl;
+    for (int i = a; i <= b; ++i) {
+        if (i % 2 == 0) sum += i;
+    }
+
+    cout << "Sum of even numbers between " << a << " and " << b << " = " << sum << endl;
 }
 
 int main() {
     sumEvenNumbers();
     return 0;
 }
-
