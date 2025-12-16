02981c93bd3875dff0532a0f48d6cd2d762b06fa
Mortalzs <kleyzormax7@gmail.com>
2025-12-16 16:42:42 +0000

    Add new homework 2: string length

---
+++ newhomework2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    cout << "Length of the string: " << input.length() << endl;
+
+    return 0;
+}
