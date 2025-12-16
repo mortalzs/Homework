2bdb3be75bb6b9ca165bf8ebffdb5c51aaaf4840
Mortalzs <kleyzormax7@gmail.com>
2025-12-16 16:36:57 +0000

    Add ASCII art program

---
+++ ascii_art.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "      /\\_/\\      " << endl;
+    cout << "     ( o.o )     " << endl;
+    cout << "      > ^ <      " << endl;
+    cout << endl;
+    cout << "   ASCII ART CAT " << endl;
+    return 0;
+}
