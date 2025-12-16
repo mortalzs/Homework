5022e6183bc2490570c934d9dec0c78d475af4a3
Mortalzs <kleyzormax7@gmail.com>
2025-12-16 14:36:14 +0000

    Add ASCII art project

---
+++ ascii_art_project/ascii_art.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "  /\\_/\\  " << endl;
+    cout << " ( o.o ) " << endl;
+    cout << "  > ^ <  " << endl;
+    return 0;
+}
