efe3b7964ae5b2b7f41d79ceeffd3e1c8591d7b1
Mortalzs <kleyzormax7@gmail.com>
2025-12-16 17:15:56 +0000

    Add homework3

---
+++ newhomework4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int mid = n / 2;
+
+    for (int i = 0; i <= mid; i++) {
+        for (int j = 0; j < mid - i; j++) cout << "  ";
+        for (int j = 0; j < 2 * i + 1; j++) cout << "* ";
+        cout << endl;
+    }
+
+    for (int i = mid - 1; i >= 0; i--) {
+        for (int j = 0; j < mid - i; j++) cout << "  ";
+        for (int j = 0; j < 2 * i + 1; j++) cout << "* ";
+        cout << endl;
+    }
+
+    return 0;
+}
